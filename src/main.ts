c3cc60b5c9fc4c4299fd761cfeff3b1668868656
OlehMikhailychenko <devlydexter@gmail.com>
2022-10-22 02:23:15 +0300

    cors v7

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,23 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const options = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+  const corsOptions: CorsOptions = {
+    allowedHeaders: [
+      'origin',
+      'x-requested-with',
+      'content-type',
+      'accept',
+      'authorization',
+    ],
     credentials: true,
+    origin: ['http://localhost:3001', 'http://localhost:3000'],
   };
+  app.enableCors(corsOptions);
 
-  app.enableCors(options);
   // const configService = app.get(ConfigService);
   // const PORT = configService.get('PORT');
   app.useGlobalPipes(new ValidationPipe());
